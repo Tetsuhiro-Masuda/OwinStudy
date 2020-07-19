4474ea2ccb765a6734057cf2d0cbe33be38329fa
Tetsu <54490996+Tetsuhiro-Masuda@users.noreply.github.com>
2020-07-19 17:59:18 +0900

    リライト処理実装 (#5)
    * 不要ファイル削除
    
    * ファイルを出力するように変更
    
    * デフォルトファイルとディレクトリブラウザの指定
    
    * リライト処理追加

--- SelfHostServer/StartUp.cs
+++ SelfHostServer/StartUp.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Owin;
 using Microsoft.Owin.FileSystems;
 using Microsoft.Owin.StaticFiles;
 using Owin;
@@ -8,19 +11,41 @@ namespace SelfHostServer
 {
     internal class StartUp
     {
+        private readonly PathString _applicationPath;
+
         public StartUp()
         {
-            //need reflection
+            _applicationPath = new PathString("/Contents1");
+            //_applicationPath = new PathString("/Contents1/Contents2");
         }
 
         //use https://docs.microsoft.com/ja-jp/aspnet/aspnet/overview/owin-and-katana/owin-startup-class-detection
         public void Configuration(IAppBuilder app)
         {
+            //applicationPath 以下の呼び出しに読み替える
+            // http://localhost:9000/testPicture.png → http://localhost:9000/Contents1/testPicture.png
+            app.Use<RewriteMiddleWere>(_applicationPath);
+
+            // details https://docs.microsoft.com/ja-jp/aspnet/core/fundamentals/static-files?view=aspnetcore-3.1
             var fileServerOption = new FileServerOptions
             {
-                FileSystem = new PhysicalFileSystem(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "webroot"))
+                FileSystem = new PhysicalFileSystem(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "webroot")),
+                EnableDirectoryBrowsing = true,
             };
+            fileServerOption.StaticFileOptions.OnPrepareResponse = ctx =>
+            {
+                //キャッシュを削除しておかないと誤った応答が返り続けることがある
+                ctx.OwinContext.Response.Headers["Cache-Control"] = "no-cache, no-store";
+            };
+
+            fileServerOption.DefaultFilesOptions.DefaultFileNames = new List<string> { Path.Combine("testPicture.png") };
             app.UseFileServer(fileServerOption);
+
+            //fileServer = {staticFile,defaultFile,DirectoryBrowser}　下３つの複合
+            //staticFile = provide file contents ファイルだけ提供したければこれで十分
+            //defaultFile = rewrite {default.htm,default.html,indexhtm,index.html} or my entry point
+            // webroot内のフォルダ要求に対し既定or指定のファイルを返すようにする
+            //directoryBrowser = allow directory referrence ブラウザからディレクトリ構成が見れるようになる
 
             app.Use((context, next) =>
             {
@@ -41,6 +66,22 @@ namespace SelfHostServer
         string getTime()
         {
             return DateTime.Now.Millisecond.ToString();
+        }
+
+        public class RewriteMiddleWere : OwinMiddleware
+        {
+            private readonly PathString _applicationPath;
+
+            public RewriteMiddleWere(OwinMiddleware next, PathString applicationPath) : base(next)
+            {
+                _applicationPath = applicationPath;
+            }
+
+            public override Task Invoke(IOwinContext context)
+            {
+                context.Request.Path = _applicationPath.Add(context.Request.Path);
+                return Next.Invoke(context);
+            }
         }
     }
 }
